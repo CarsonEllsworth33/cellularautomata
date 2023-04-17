687d7f81e6746e0259345ccfa5b8f9e28dfa97c8
Carson Ellsworth <ellsc33@yahoo.com>
2023-04-17 07:16:26 -0500

    added more rules and automata3 has interesting behaviour

--- src/automata_visualizer.py
+++ src/automata_visualizer.py
@@ -8,30 +8,33 @@ Created on Thu Apr  6 07:19:12 2023
 import curses
 import time
 import random as rng
-from automata_rules import Automata, Automata2
+from automata_rules import Rules
+import automata_rules
 POPULATED_CELL_CHAR = "*"
-EMPTY_CELL_CHAR = "-"
+EMPTY_CELL_CHAR = " "
 SLEEP_TIME = .5
 
-def automata_visualizer(stdscr,machine:Automata):
+def automata_visualizer(stdscr,machine:Rules):
     for i in range(machine.bounds[0]):
         for j in range(machine.bounds[1]):
             if(machine.matrix[i][j] == 1):
-                stdscr.addstr(i,j, POPULATED_CELL_CHAR)
+                stdscr.addch(i,j, POPULATED_CELL_CHAR)
             else:
-                stdscr.addstr(i,j, EMPTY_CELL_CHAR)
+                stdscr.addch(i,j, EMPTY_CELL_CHAR)
+                pass
     
     
 
 # Clear Screen
 def main(stdscr):
+    curses.start_color()
     stdscr.clear()
     dim = curses.LINES, curses.COLS#screen dimensions in x,y
     #-1 is quick fix for curses bottom right being unwritable
     matrix = [[rng.randint(0,1) for _ in range(dim[1]-1)] for _ in range(dim[0]-1)]
     
     
-    mach = Automata2(matrix)
+    mach = automata_rules.Automata3(matrix)
     try:
         while True:
             automata_visualizer(stdscr,mach)
